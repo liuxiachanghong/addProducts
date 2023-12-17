a1a7b877af5b9242a874a3cd463453b27a29ee0d
liuxiachanghong <liuxiachanghong@gmail.com>
2023-12-17 18:05:42 +0900

    addProducts components (lwc)

---
+++ .sfdx/typings/lwc/apex/ProductSearchController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/ProductSearchController.getPricebookEntries" {
+  export default function getPricebookEntries(param: {pricebook2Id: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ProductSearchController.createLineItems" {
+  export default function createLineItems(param: {lineItems: any}): Promise<any>;
+}
