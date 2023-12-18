091d9b3526498d8c2aed6d439a9edd965a85d3f9
liuxiachanghong <liuxiachanghong@gmail.com>
2023-12-18 10:07:29 +0900

    Close modal by CloseActionScreenEvent action

--- force-app/main/default/lwc/productItems/productItems.js
+++ force-app/main/default/lwc/productItems/productItems.js
@@ -1,103 +1,140 @@
-import { LightningElement, api, track } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LightningElement, api, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-import createLineItems from '@salesforce/apex/ProductSearchController.createLineItems';
+import createLineItems from "@salesforce/apex/ProductSearchController.createLineItems";
 
 export default class ProductItems extends LightningElement {
-    @api recordId;
-    @api records;
-    @track draftValues = [];
-    @track createButtonDisabled = false;
-    @track isLoading = false;
+  @api recordId;
+  @api records;
+  @track draftValues = [];
+  @track createButtonDisabled = false;
+  @track isLoading = false;
 
-    @track columns = [
-        { label: 'Product Name', fieldName: 'Name', editable: false },
-        { label: 'Sales Price', fieldName: 'ListPrice', type: 'currency', editable: true },
-        { label: 'Quantity', fieldName: 'Quantity', type: 'number', editable: true },
-        { label: 'Total', fieldName: 'Total', type: 'currency' }
-    ];
+  @track columns = [
+    { label: "Product Name", fieldName: "Name", editable: false },
+    {
+      label: "Sales Price",
+      fieldName: "ListPrice",
+      type: "currency",
+      editable: true,
+    },
+    {
+      label: "Quantity",
+      fieldName: "Quantity",
+      type: "number",
+      editable: true,
+    },
+    { label: "Total", fieldName: "Total", type: "currency" },
+  ];
 
-    handleSave(event) {
-        this.draftValues = event.detail.draftValues;
-        const updatedRecords = this.records.map(record => {
-        const draftUpdate = this.draftValues.find(draft => draft.Id === record.Id);
-            if (draftUpdate) {
-                let newTotal = record.Total;
-                if (draftUpdate.Quantity !== undefined || draftUpdate.ListPrice !== undefined) {
-                    const newQuantity = draftUpdate.Quantity !== undefined ? draftUpdate.Quantity : record.Quantity;
-                    const newListPrice = draftUpdate.ListPrice !== undefined ? draftUpdate.ListPrice : record.ListPrice;
-                    newTotal = newQuantity * newListPrice;
-                }
-                return {...record, ...draftUpdate, Total: newTotal};
-            }
-            return record;
-        });
-        this.records = updatedRecords;
-        this.createButtonDisabled = false;
-        this.draftValues = [];
-    }
+  handleSave(event) {
+    this.draftValues = event.detail.draftValues;
+    const updatedRecords = this.records.map((record) => {
+      const draftUpdate = this.draftValues.find(
+        (draft) => draft.Id === record.Id
+      );
+      if (draftUpdate) {
+        let newTotal = record.Total;
+        if (
+          draftUpdate.Quantity !== undefined ||
+          draftUpdate.ListPrice !== undefined
+        ) {
+          const newQuantity =
+            draftUpdate.Quantity !== undefined
+              ? draftUpdate.Quantity
+              : record.Quantity;
+          const newListPrice =
+            draftUpdate.ListPrice !== undefined
+              ? draftUpdate.ListPrice
+              : record.ListPrice;
+          newTotal = newQuantity * newListPrice;
+        }
+        return { ...record, ...draftUpdate, Total: newTotal };
+      }
+      return record;
+    });
+    this.records = updatedRecords;
+    this.createButtonDisabled = false;
+    this.draftValues = [];
+  }
 
-    handleCellChange(event) {
-        this.createButtonDisabled = true;
-        this.draftValues = event.detail.draftValues;
-        const updatedRecords = this.records.map(record => {
-        const draftUpdate = this.draftValues.find(draft => draft.Id === record.Id);
-            if (draftUpdate) {
-                let newTotal = record.Total;
-                if (draftUpdate.Quantity !== undefined || draftUpdate.ListPrice !== undefined) {
-                    const newQuantity = draftUpdate.Quantity !== undefined ? draftUpdate.Quantity : record.Quantity;
-                    const newListPrice = draftUpdate.ListPrice !== undefined ? draftUpdate.ListPrice : record.ListPrice;
-                    newTotal = newQuantity * newListPrice;
-                }
-                return {...record, ...draftUpdate, Total: newTotal};
-            }
-            return record;
-        });
-        this.records = updatedRecords;
-    }
+  handleCellChange(event) {
+    this.createButtonDisabled = true;
+    this.draftValues = event.detail.draftValues;
+    const updatedRecords = this.records.map((record) => {
+      const draftUpdate = this.draftValues.find(
+        (draft) => draft.Id === record.Id
+      );
+      if (draftUpdate) {
+        let newTotal = record.Total;
+        if (
+          draftUpdate.Quantity !== undefined ||
+          draftUpdate.ListPrice !== undefined
+        ) {
+          const newQuantity =
+            draftUpdate.Quantity !== undefined
+              ? draftUpdate.Quantity
+              : record.Quantity;
+          const newListPrice =
+            draftUpdate.ListPrice !== undefined
+              ? draftUpdate.ListPrice
+              : record.ListPrice;
+          newTotal = newQuantity * newListPrice;
+        }
+        return { ...record, ...draftUpdate, Total: newTotal };
+      }
+      return record;
+    });
+    this.records = updatedRecords;
+  }
 
-    handleCreate(){
-        this.isLoading = true;
-        const remappedRecords = this.transformDataForOpportunityLineItems();
-        
-        createLineItems({ lineItems: remappedRecords })
-            .then(result => {
-                this.showToast('Success', 'Pricebook Entries Created Successfully', 'success', 'pester');
-                this.handleCloseDatatable();
-                this.isLoading = false;
-            })
-            .catch(error => {
-                console.error('Error in creating records:', error);
-            });
-    }
+  handleCreate() {
+    this.isLoading = true;
+    const remappedRecords = this.transformDataForOpportunityLineItems();
 
-    transformDataForOpportunityLineItems() {
-        return this.records.map(record => {
-            const item = {
-                OpportunityId       : this.recordId,
-                PricebookEntryId    : record.Id,
-                Name                : record.Name,
-                Quantity            : record.Quantity,
-                UnitPrice           : record.ListPrice,
-            };
-            return item;
-        });
-    }
+    createLineItems({ lineItems: remappedRecords })
+      .then((result) => {
+        this.showToast(
+          "Success",
+          "Pricebook Entries Created Successfully",
+          "success",
+          "pester"
+        );
+        this.handleCloseDatatable();
+        this.isLoading = false;
+      })
+      .catch((error) => {
+        console.error("Error in creating records:", error);
+      });
+  }
 
-    showToast(title, message, variant, mode) {
-        const toastEvent = new ShowToastEvent({
-            title: title,
-            message: message,
-            variant: variant,
-            mode: mode,
-        });
-        this.dispatchEvent(toastEvent);
-    }
+  transformDataForOpportunityLineItems() {
+    return this.records.map((record) => {
+      const item = {
+        OpportunityId: this.recordId,
+        PricebookEntryId: record.Id,
+        Name: record.Name,
+        Quantity: record.Quantity,
+        UnitPrice: record.ListPrice,
+      };
+      return item;
+    });
+  }
 
-    handleCloseDatatable(){
-        const event = new CustomEvent('closedatatable', {
-            detail: { showDatatable: false, showSuccess:true }
-        });
-        this.dispatchEvent(event);
-    }
+  showToast(title, message, variant, mode) {
+    const toastEvent = new ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant,
+      mode: mode,
+    });
+    this.dispatchEvent(toastEvent);
+  }
+
+  handleCloseDatatable() {
+    const event = new CustomEvent("closedatatable", {
+      detail: { showDatatable: false },
+    });
+    this.dispatchEvent(event);
+  }
 }
--- force-app/main/default/lwc/productSearch/productSearch.js
+++ force-app/main/default/lwc/productSearch/productSearch.js
@@ -1,91 +1,94 @@
-import { LightningElement, wire, track, api } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
-import getPricebookEntries from '@salesforce/apex/ProductSearchController.getPricebookEntries';
-import OPPORTUNITY_PRICEBOOK2ID_FIELD from '@salesforce/schema/Opportunity.Pricebook2Id';
-
+import { LightningElement, wire, track, api } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import getPricebookEntries from "@salesforce/apex/ProductSearchController.getPricebookEntries";
+import OPPORTUNITY_PRICEBOOK2ID_FIELD from "@salesforce/schema/Opportunity.Pricebook2Id";
+import { CloseActionScreenEvent } from "lightning/actions";
 
 export default class ProductTreeGrid extends LightningElement {
-    @api recordId;
+  @api recordId;
 
-    @track columns = [
-        { label: 'Name', fieldName: 'Name' },
-        { label: 'Family', fieldName: 'Family' },
-        { label: 'List Price', fieldName: 'ListPrice', type: 'currency' }
-    ];
+  @track columns = [
+    { label: "Name", fieldName: "Name" },
+    { label: "Family", fieldName: "Family" },
+    { label: "List Price", fieldName: "ListPrice", type: "currency" },
+  ];
 
-    @track gridData;
-    @track showTreeGrid = true;
-    @track showDatatable = false;
-    @track records;
+  @track gridData;
+  @track showTreeGrid = true;
+  @track showDatatable = false;
+  @track records;
 
-    opportunityPricebook2Id;
-    wiredOpportunityResult;
+  opportunityPricebook2Id;
+  wiredOpportunityResult;
 
-    @wire(getRecord, { recordId: '$recordId', fields: [OPPORTUNITY_PRICEBOOK2ID_FIELD] })
-    wiredOpportunity({ data, error }) {
-        if (data) {
-            this.wiredOpportunityResult = data;
-            this.opportunityPricebook2Id = data.fields.Pricebook2Id.value;
-            this.loadPricebookEntries();
-        } else if (error) {
-            console.log('wiredOpportunity error:', error);
-        }
+  @wire(getRecord, {
+    recordId: "$recordId",
+    fields: [OPPORTUNITY_PRICEBOOK2ID_FIELD],
+  })
+  wiredOpportunity({ data, error }) {
+    if (data) {
+      this.wiredOpportunityResult = data;
+      this.opportunityPricebook2Id = data.fields.Pricebook2Id.value;
+      this.loadPricebookEntries();
+    } else if (error) {
+      console.log("wiredOpportunity error:", error);
     }
+  }
 
-    loadPricebookEntries() {
-        getPricebookEntries({ pricebook2Id: this.opportunityPricebook2Id })
-            .then(result => {
-                this.gridData = this.transformData(result);
-            })
-            .catch(error => {
-                console.log('getPricebookEntries error:', error);
-            });
-    }
+  loadPricebookEntries() {
+    getPricebookEntries({ pricebook2Id: this.opportunityPricebook2Id })
+      .then((result) => {
+        this.gridData = this.transformData(result);
+      })
+      .catch((error) => {
+        console.log("getPricebookEntries error:", error);
+      });
+  }
 
-    transformData(data) {
-        let treeData = [];
-        let families = new Map();
+  transformData(data) {
+    let treeData = [];
+    let families = new Map();
 
-        data.forEach(entry => {
-            let family = entry.Product2.Family;
-            if (!families.has(family)) {
-                families.set(family, {
-                    _children: [],
-                    Id          : family, // Use Family as a unique key
-                    Name        : family, // Group by Family
-                    Family      : '',
-                    ListPrice   : null,
-                    Quantity    : null,
-                    Total       : null
-                });
-            }
-            families.get(family)._children.push({
-                Id                  : entry.Id,
-                Name                : entry.Product2.Name,
-                Family              : family,
-                ListPrice           : entry.UnitPrice,
-                Quantity            : 1,
-                Total               : entry.UnitPrice * 1,
-            });
+    data.forEach((entry) => {
+      let family = entry.Product2.Family;
+      if (!families.has(family)) {
+        families.set(family, {
+          _children: [],
+          Id: family, // Use Family as a unique key
+          Name: family, // Group by Family
+          Family: "",
+          ListPrice: null,
+          Quantity: null,
+          Total: null,
         });
+      }
+      families.get(family)._children.push({
+        Id: entry.Id,
+        Name: entry.Product2.Name,
+        Family: family,
+        ListPrice: entry.UnitPrice,
+        Quantity: 1,
+        Total: entry.UnitPrice * 1,
+      });
+    });
 
-        families.forEach((value, key) => {
-            treeData.push(value);
-        });
+    families.forEach((value, key) => {
+      treeData.push(value);
+    });
 
-        return treeData;
-    }
+    return treeData;
+  }
 
-    handleSelectAction() {
-        const grid = this.template.querySelector('lightning-tree-grid');
-        const selectedRows = grid.getSelectedRows();
-        this.records = selectedRows; 
-        this.showTreeGrid = false; 
-        this.showDatatable = true;
-    }
+  handleSelectAction() {
+    const grid = this.template.querySelector("lightning-tree-grid");
+    const selectedRows = grid.getSelectedRows();
+    this.records = selectedRows;
+    this.showTreeGrid = false;
+    this.showDatatable = true;
+  }
 
-    handleFinish(event) {
-        this.showDatatable = event.detail.showDatatable;
-        this.showSuccess = event.detail.showSuccess;
-    }
+  handleFinish(event) {
+    this.showDatatable = event.detail.showDatatable;
+    this.dispatchEvent(new CloseActionScreenEvent());
+  }
 }
